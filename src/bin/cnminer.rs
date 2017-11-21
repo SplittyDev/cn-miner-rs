d99318564f05011d6b8a96b1bde3e1fd57d3ab0c
SplittyDev <splittydev@gmail.com>
2017-11-21 03:36:21 +0100

    Update binary

--- src/bin/cnminer.rs
+++ src/bin/cnminer.rs
@@ -2,6 +2,6 @@ extern crate cnminer;
 
 fn main() {
     let mut output = [0u8; 32];
-    ::cnminer::cryptonight(b"", &mut output[..]);
+    ::cnminer::cryptonight(b"This is a test", &mut output[..]);
     output.iter().for_each(|b| print!("{:x}", b));
 }
