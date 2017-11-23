9c5b02a78d5dd348d1e58b579bc88042592084ce
SplittyDev <splittydev@gmail.com>
2017-11-23 10:29:49 +0100

    Add benchmarks

---
+++ benches/cryptonight.rs
@@ -0,0 +1,21 @@
+extern crate cnminer;
+
+#[macro_use]
+extern crate bencher;
+use bencher::Bencher;
+
+extern crate rand;
+use rand::Rng;
+
+fn bench_cryptonight(bench: &mut Bencher) {
+    let mut rng = rand::thread_rng();
+    let mut input = [0u8; 128];
+    bench.iter(|| {
+        rng.fill_bytes(&mut input);
+        let mut output = [0u8; 32];
+        ::cnminer::cryptonight(input.as_ref(), &mut output[..]);
+    });
+}
+
+benchmark_group!(benches, bench_cryptonight);
+benchmark_main!(benches);
